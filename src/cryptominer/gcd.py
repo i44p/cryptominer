07038443e087c31bee3054987d051f49e00c001c
i44p <jsbbdjzjsjawk+@gmail.com>
2023-09-23 19:02:32 +0300

    fix euclidean_recursive, implement euclidean

--- src/cryptominer/gcd.py
+++ src/cryptominer/gcd.py
@@ -1,5 +1,6 @@
 import inspect
 
+
 def is_prime(num: int) -> bool:
     if num % 2 == 0:
         if num == 2:
@@ -10,15 +11,23 @@ def is_prime(num: int) -> bool:
             return False
     return True
 
+
 class GreatestCommonDivisor:
     def __init__(self, a, b):
         self.a = a
         self.b = b
-    
+
     def Euclidean_recursive(self) -> int:
         mod, remainder = divmod(self.a, self.b)
-        if remainder == 0:
+        if mod == 0:
             return self.a
         return GreatestCommonDivisor(self.b, mod).Euclidean_recursive()
 
+    def Euclidean(self) -> int:
+        mod, remainder = divmod(self.a, self.b)
+        remainder_last = self.b
+        while remainder != 0:
+            remainder_last = remainder
+            mod, remainder = divmod(remainder_last, mod)
 
+        return remainder_last
