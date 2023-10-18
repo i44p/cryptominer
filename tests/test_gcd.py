ff34f491356dbf68ffb35f9cae3f73ac1c62d4c8
i44p <jsbbdjzjsjawk+@gmail.com>
2023-10-18 20:42:32 +0300

    remove coprime since it's the same as gcd

--- tests/test_gcd.py
+++ tests/test_gcd.py
@@ -21,6 +21,7 @@ def test_is_prime(test_input, expected):
 
 @pytest.mark.parametrize("test_input,expected", [
     (cryptominer.GreatestCommonDivisor(1234, 54), 2)
+    (cryptominer.GreatestCommonDivisor(2548, 22), 2)
 ])
 def test_gcd(test_input, expected):
     AssertFor(test_input, expected)
