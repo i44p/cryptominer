4290c6c66686307abfd863c9ef42811da8b5fe2c
i44p <jsbbdjzjsjawk+@gmail.com>
2023-10-18 23:31:56 +0300

    make prime calc faster using sqrt, add dumb implementation, add tests

--- src/cryptominer/factorization.py
+++ src/cryptominer/factorization.py
@@ -1,18 +1,32 @@
+from math import sqrt as _sqrt
+
+
 def is_prime(num: int) -> bool:
     if num % 2 == 0:
         if num == 2:
             return True
         return False
-    for i in range(3, num // 2, 2):
+    for i in range(3, int(_sqrt(num)) + 1, 2):
         if num % i == 0:
             return False
     return True
 
 
 class Factorization:
-    def __init__(self, a, b):
+    def __init__(self, a):
         self.a = a
-        self.b = b
     
-    def Dumb():
-        ...
+    def Dumb(self):
+        num = self.a
+        factors = ()
+        
+        while num % 2 == 0:
+            factors += (2,)
+            num //= 2
+            
+        for factor in range(3, int(_sqrt(num)) + 1, 2):
+            while num % factor == 0:
+                factors += (factor,)
+                num //= factor
+
+        return factors
--- tests/test_factorization.py
+++ tests/test_factorization.py
@@ -14,7 +14,17 @@ from helpers import *
     (17, True),
     (23, True),
     (25, False),
-    (6_700_417, True)
+    (6_700_417, True),
+    (81_057_226_637, False)
 ])
 def test_is_prime(test_input, expected):
     assert cryptominer.is_prime(test_input) == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    # (cryptominer.Coprimes(1234, 54), 2)
+    (cryptominer.Factorization(82_798_848), (2,) * 8 + (3,) * 5 + (11,) * 3),
+    (cryptominer.Factorization(81_057_226_635), (3,) * 3 + (5,) + (7,) * 3 + (11,) * 2 + (17,) + (23,) + (37,))
+])
+def test_factorization(test_input, expected):
+    AssertFor(test_input, expected)
