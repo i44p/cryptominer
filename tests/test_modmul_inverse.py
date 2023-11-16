8705d930772ba5275979944d11a5e99e06b4fab9
i44p <jsbbdjzjsjawk+@gmail.com>
2023-11-16 12:00:10 +0300

    add modmul testcases

--- tests/test_modmul_inverse.py
+++ tests/test_modmul_inverse.py
@@ -8,6 +8,8 @@ from helpers import *
     (cryptominer.ModularMultiplicativeInverse(5, 11), 9),
     (cryptominer.ModularMultiplicativeInverse(2, 7), 4),
     (cryptominer.ModularMultiplicativeInverse(6, 7), 6),
+    (cryptominer.ModularMultiplicativeInverse(7, 5), 3),
+    (cryptominer.ModularMultiplicativeInverse(11, 15), 11),
 ])
 def test_modmul_inverse(test_input, expected):
     AssertFor(test_input, expected)
