61b6f9887ce08436a48d0a2ac274242ca0f9c073
i44p <jsbbdjzjsjawk+@gmail.com>
2023-11-16 13:48:42 +0300

    update RSA test

--- tests/test_RSA.py
+++ tests/test_RSA.py
@@ -5,13 +5,14 @@ from helpers import *
 
 
 def test_RSA():
-    message = 'ИВАН ГОРЫНЫЧ'
-    for _ in range(10):
-        public_key, private_key = cryptominer.KeypairRSA(pq_size=8).Generate()
-        
-        # public_key, private_key = ((158413, 428786), (158581, 428786))
-        
+    message = 'ЗМЕЙ ГОРЫНЫЧ'
+    for _ in range(1):
+        public_key, private_key = cryptominer.KeypairRSA(pq_size=24).Generate()
+
+        # max encode size = (1<<pq_size)**2
+        # public_key, private_key = ((55847289989431, 948425708075781), (394439629876615, 948425708075781))
+
         encoded = cryptominer.EncodeRSA(message, public_key).Cipher()
         decoded = cryptominer.DecodeRSA(encoded, private_key).Decipher()
-        
+
         assert message == decoded
